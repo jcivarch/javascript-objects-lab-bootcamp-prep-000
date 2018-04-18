5b12f60aad1d2917f109c904831481bd6f244cf0
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-18 02:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object[key] = value;
+  object[key] = value;
+
+  return object;
 }
